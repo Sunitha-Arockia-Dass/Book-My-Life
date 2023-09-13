0ffbdcc6b5f4552da3a3c7fc82f93e9880e6169a
Thomas Lebas <thomas.lebas26@gmail.com>
2023-09-13 13:44:19 +0200

    db security

--- db/index.js
+++ db/index.js
@@ -1,20 +1,20 @@
 // ℹ️ package responsible to make the connection with mongodb
 // https://www.npmjs.com/package/mongoose
-const mongoose = require("mongoose");
+const mongoose = require("mongoose")
 
 // ℹ️ Sets the MongoDB URI for our app to have access to it.
 // If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
 
 const MONGO_URI =
-  process.env.MONGODB_URI || "mongodb+srv://Sunitha:samyu143@cluster0.wlo19f2.mongodb.net/localhost:27017";
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/Book-My-Life"
 
 
 mongoose
   .connect(MONGO_URI)
   .then((x) => {
-    const databaseName = x.connections[0].name;
-    console.log(`Connected to Mongo! Database name: "${databaseName}"`);
+    const databaseName = x.connections[0].name
+    console.log(`Connected to Mongo! Database name: "${databaseName}"`)
   })
   .catch((err) => {
-    console.error("Error connecting to mongo: ", err);
-  });
+    console.error("Error connecting to mongo: ", err)
+  })
